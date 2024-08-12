95a78822a5e33795e6d0fbcf5f881c601983b9f1
mukhlisakb <mukhlisakb@gmail.com>
2024-08-12 09:32:49 +0700

    add container and column widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,59 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const ShuffleSongApp());
+}
+
+class ShuffleSongApp extends StatelessWidget {
+  const ShuffleSongApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Shuffle Song Apps",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const ShuffleSongWidget());
+  }
+}
+
+class ShuffleSongWidget extends StatelessWidget {
+  const ShuffleSongWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(
+            width: 0.5,
+          )),
+          padding: const EdgeInsets.all(10),
+          width: 200,
+          height: 200,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/hindia.jpeg",
+                width: 140,
+                height: 140,
+              ),
+              const SizedBox(height: 12),
+              const Text(
+                "Kita ke sana - Hindia",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

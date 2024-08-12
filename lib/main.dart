7c6145a3924e8bce4a722ee8bfda2b5693b7c41b
mukhlisakb <mukhlisakb@gmail.com>
2024-08-12 13:15:01 +0700

    feat: add row widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,63 @@ class ShuffleSongApp extends StatelessWidget {
   }
 }
 
+class TestSongWidget extends StatelessWidget {
+  const TestSongWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.teal,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.orange,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class ShuffleSongWidget extends StatelessWidget {
   const ShuffleSongWidget({super.key});
 
@@ -29,6 +86,26 @@ class ShuffleSongWidget extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/spotify.png",
+                  width: 36,
+                  height: 36,
+                ),
+                const SizedBox(width: 4),
+                const Text(
+                  "Spotify",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
             Center(
               child: Container(
                 decoration: BoxDecoration(

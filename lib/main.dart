4078d80c1b372415a60231c0a6284b5338584b40
mukhlisakb <mukhlisakb@gmail.com>
2024-08-12 09:43:42 +0700

    feat: add icon widget

--- lib/main.dart
+++ lib/main.dart
@@ -24,34 +24,47 @@ class ShuffleSongWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(
-            width: 0.5,
-          )),
-          padding: const EdgeInsets.all(10),
-          width: 200,
-          height: 200,
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/hindia.jpeg",
-                width: 140,
-                height: 140,
-              ),
-              const SizedBox(height: 12),
-              const Text(
-                "Kita ke sana - Hindia",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  width: 0.5,
+                )),
+                padding: const EdgeInsets.all(10),
+                width: 200,
+                height: 200,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/images/hindia.jpeg",
+                      width: 140,
+                      height: 140,
+                    ),
+                    const SizedBox(height: 12),
+                    const Text(
+                      "Kita ke sana - Hindia",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
-                textAlign: TextAlign.center,
-              )
-            ],
-          ),
+              ),
+            ),
+            Icon(
+              Icons.bookmark_add_rounded,
+              color: Colors.blue,
+              size: 48,
+            )
+          ],
         ),
       ),
     );
